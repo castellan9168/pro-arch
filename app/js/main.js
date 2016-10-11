131b4787d7f5495f3ee33e98a311ef9826688f42
castellan9168 <m_ziolo_86@o2.pl>
2016-10-11 20:26:54 +0200

    Test deploy

--- app/js/main.js
+++ app/js/main.js
@@ -1,3 +1,4 @@
+console.log('No niby jestem i co z tego?!');
 var app = angular.module('proArch', []);
 app.controller('proArchCtrl', function($scope){
     $scope.message = 'Angular is pretty cool.';
