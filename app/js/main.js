5a7d817798bc29f7793e84d7b5dee4c26367b18d
castellan9168 <m_ziolo_86@o2.pl>
2016-10-12 18:26:35 +0200

    Subpages loaded via ngRoute

--- app/js/main.js
+++ app/js/main.js
@@ -1,4 +1,19 @@
 var app = angular.module('proArch', ['ngRoute']);
+app.config(['$routeProvider', function($routeProvider){
+    $routeProvider
+    .when('/main', {
+        templateUrl : 'html/main.html'
+    })
+    .when('/projects', {
+        templateUrl : 'html/projects.html'
+    })
+    .when('/office', {
+        templateUrl : 'html/office.html'
+    })
+    .when('/contact', {
+        templateUrl : 'html/contact.html'
+    });
+}]);
 app.controller('proArchCtrl',[ '$scope', '$route', function($scope, $route){
     $scope.message = 'Angular is pretty cool.';
 }]);
